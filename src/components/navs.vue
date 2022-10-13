b35566ffb9a5412700f330d7a1fa73587a7a3716
leeokhee <pixellog@gmail.com>
2022-10-13 11:43:34 +0900

    docs add

--- src/components/navs.vue
+++ src/components/navs.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="navbar">
-    <div class="container py-2">
+    <div class="container">
       <div class="d-flex justify-content-between w-100">
         <a href="/" class="text-white font-weight-black fs-4 text-decoration-none">RNB</a>
         <div class="hstack gap-3">
@@ -8,40 +8,16 @@
           <a class="link-light text-decoration-none" href="/">이력검색</a>
           <a class="link-light text-decoration-none" href="/">설정</a>
           <a class="link-light text-decoration-none" href="/guide.html">가이드</a>
-          <button class="navbar-toggler white" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-            <span class="navbar-toggler-icon"></span>
+          <button class="btn btn-light rounded" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span class="material-symbols-outlined lh-base">menu</span>
           </button>
           <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+              <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
               <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
-              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li>
-                      <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
-              </ul>
-              <form class="d-flex mt-3" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
+              ...
             </div>
           </div>
         </div>
