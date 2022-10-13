bc7218e21aa086d2690341306a96964e8fd1b8b1
leeokhee <pixellog@gmail.com>
2022-10-13 16:46:13 +0900

    docs add

--- src/components/navs.vue
+++ src/components/navs.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="navbar">
     <div class="container">
-      <div class="d-flex justify-content-between w-100">
-        <a href="/" class="text-white font-weight-black fs-4 text-decoration-none">RNB</a>
+      <div class="d-flex justify-content-between align-center w-100">
+        <a href="/" class="font-weight-black nav-link fs-4 text-decoration-none bi">RNB</a>
         <div class="hstack gap-3">
-          <a class="link-light text-decoration-none" href="/">대시보드</a>
-          <a class="link-light text-decoration-none" href="/">이력검색</a>
-          <a class="link-light text-decoration-none" href="/">설정</a>
-          <a class="link-light text-decoration-none" href="/guide.html">가이드</a>
-          <button class="btn btn-light rounded" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+          <a class="link-secondary text-decoration-none" href="/dashboard.html">대시보드</a>
+          <a class="link-secondary text-decoration-none" href="/guide.html">이력검색</a>
+          <a class="link-secondary text-decoration-none" href="/">설정</a>
+
+          <button class="btn rounded" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
             <span class="material-symbols-outlined lh-base">menu</span>
           </button>
           <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
@@ -17,29 +17,24 @@
               <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
-              ...
+              <a href="/guide.html">가이드</a>
             </div>
           </div>
+
+          <div class="dropdown">
+            <a href="#none" data-bs-toggle="dropdown" aria-expanded="false">
+              <img class="rounded-pill"
+                   src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfDB8MHx8&auto=format&fit=crop&w=36&h=36"/>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end text-caption">
+              <li><a class="dropdown-item" href="#">Profile</a></li>
+              <li><a class="dropdown-item" href="#">Settings</a></li>
+              <li><a class="dropdown-item" href="#">Logout</a></li>
+            </ul>
+          </div>
+
         </div>
       </div>
     </div>
   </div>
-
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-    }
-  },
-}
-</script>
-
-<style scoped>
-.navbar {
-  background-color: #272660;
-  z-index: 100;
-  top: 0;
-}
-</style>
